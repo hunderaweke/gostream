7afac69145df7d88532d96779009c9808b7d195d
Hundera Awoke <hunderaweke@gmail.com>
2025-11-25 12:33:08 +0300

    update: minio return type fixing

--- internal/database/minio.go
+++ internal/database/minio.go
@@ -7,10 +7,10 @@ import (
 	"github.com/minio/minio-go/v7/pkg/credentials"
 )
 
-func GetMinio() (minio.Client, error) {
+func GetMinio() (*minio.Client, error) {
 	clnt, err := minio.New(os.Getenv("MINIO_ENDPOINT"), &minio.Options{
 		Creds:  credentials.NewStaticV4(os.Getenv("MINIO_ACCESS_ID"), os.Getenv("MINIO_SECRET_ACCESS_KEY"), ""),
 		Secure: true,
 	})
-	return *clnt, err
+	return clnt, err
 }
